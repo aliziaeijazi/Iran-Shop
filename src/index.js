b26943f979709ad00039c0fdf0ca1ecaa14ec754
aliziaeijazi <aliziaeijazi@gmail.com>
2021-07-12 08:27:47 +0430

    solve issue

--- src/index.js
+++ src/index.js
@@ -4,15 +4,18 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import {AppRoute} from "./route/App.route";
 import {ToastContainer} from "react-toastify";
+import { create } from 'jss';
+import rtl from 'jss-rtl';
+import { StylesProvider, jssPreset } from '@material-ui/core/styles';
 
 ReactDOM.render(
     <React.StrictMode>
-        <>
+        <RTL>
             <AppRoute/>
             <ToastContainer position="bottom-left" autoClose={5000} hideProgressBar={false} newestOnTop={false}
                             closeOnClick
                             rtl={true} pauseOnFocusLoss draggable pauseOnHover/>
-        </>
+        </RTL>
     </React.StrictMode>,
     document.getElementById('root')
 );
@@ -21,3 +24,15 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+// Configure JSS
+
+function RTL(props) {
+    const jss = create({ plugins: [...jssPreset().plugins, rtl()] });
+    return (
+        <StylesProvider jss={jss}>
+            {props.children}
+        </StylesProvider>
+    );
+}
