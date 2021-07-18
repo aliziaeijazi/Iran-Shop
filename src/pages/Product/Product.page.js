3227cb5371724efa1a21604e96888ae198f2b472
aliziaeijazi <aliziaeijazi@gmail.com>
2021-07-18 13:40:06 +0430

    add button and complete this page

--- src/pages/Product/Product.page.js
+++ src/pages/Product/Product.page.js
@@ -2,9 +2,10 @@ import React, {useEffect, useState} from 'react';
 import {makeStyles} from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import {FeachProduct} from "../../api/store.api";
-import {CardMedia, Typography} from "@material-ui/core";
+import {Button, CardMedia, InputLabel, TextField, Typography} from "@material-ui/core";
 import {BASE_URL} from "../../configs/variable.config";
 import Grid from '@material-ui/core/Grid';
+import {AddShoppingCart} from "@material-ui/icons";
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -15,52 +16,66 @@ const useStyles = makeStyles((theme) => ({
         },
     },
     paper: {
-        padding:20,
+        minWidth: 400,
+        maxWidth:1400,
+        margin: 20,
+        padding: 10,
         display: "flex",
         flexDirection: "column",
-        flex: 1,
-        height: 400,
         alignItems: "center",
     },
     media: {
-        minWidth: 400,
-        minHeight: 400,
-        maxWidth: 500,
-        maxHeight: 500,
+        width: 400,
+        height: 300,
     },
     title: {
-        fontWeight: "bolder",
-        fontSize: 20,
-        color: "mediumvioletred",
+        fontSize: 18,
+        color: "#0fabc6",
         textDecoration: "none",
     },
     price: {
-        margin:50,
+        margin: "20px 0",
         display: "flex",
-        fontWeight: "bolder",
-        fontSize: 30,
-        alignItems: "center",
+        fontSize: 20,
     },
     name: {
         display: "flex",
-        fontSize: 25,
+        fontSize: 20,
         alignItems: "center",
-        textAlign:"center"
     },
     describtion: {
-        padding:40,
+        padding: " 0 20px",
         display: "flex",
         flex: 1,
         fontSize: 18,
-        alignItems: "center",
-        textAlign:"center"
     },
-    link:{
-        margin:20,
-        display:"flex",
-        width:"100%",
-        justifyContent:"flex-start",
+    link: {
+        margin: "20px 0",
+        display: "flex",
+        width: "100%",
+        justifyContent: "flex-start",
 
+    },
+    mediacard: {
+        display: "flex",
+        justifyContent: "center",
+    },
+    panel: {
+        padding: "0 20px"
+    },
+    btn: {
+        color: 'white',
+        backgroundColor: "#ef394e",
+        margin: "20px 0",
+        textAlign:"center",
+        "&:hover": {
+            backgroundColor: "#FF5E77",
+        }
+    },
+    notexist:{
+        display: "flex",
+        fontSize: 15,
+        color:"red"
     }
 }));
 
@@ -73,38 +88,44 @@ export function Product() {
         setData(data)
     }, [])
     const classes = useStyles();
-
+    const disable = !+data.count? true : false
+    console.log(data.count , disable)
     return (
-        <div >
-            <Grid container className={classes.root}>
-                <Grid item xs={10} sm={8} md={6} lg={4}>
-                    <Paper className={classes.paper} elevation={5}>
+        <div className={classes.root}>
+            <Paper elevation={20} className={classes.paper}>
+                <Grid container className={classes.root}>
+                    <Grid item sm={12} md={5} lg={5} className={classes.mediacard}>
                         <CardMedia className={classes.media}
                                    image={`${BASE_URL}${data.image}`}
                                    title={data.name}/>
-                    </Paper>
-                </Grid>
-                <Grid item xs={10} sm={8} md={6} lg={4}>
-                    <Paper className={classes.paper} elevation={5}>
-                        <Typography
-                            className={classes.name}> {data.name}
-                        </Typography>
+                    </Grid>
+                    <Grid item sm={12} md={6} lg={6} className={classes.panel}>
                         <div className={classes.link}>
                             <a className={classes.title} href={`/products?group=${data.groupname}`}>{data.groupname}</a>
                             <a className={classes.title}
-                               href={`/products?subgroup=${data.subgroupname}`}>{`  >  ${data.subgroupname} `}</a>
+                               href={`/products?subgroup=${data.subgroupname}`}>{`  /  ${data.subgroupname} `}</a>
                         </div>
+                        <Typography
+                            className={classes.name}> {data.name}
+                        </Typography>
                         <Typography
                             className={classes.price}> {`${new Number(data.price).toLocaleString("fa-IR")} تومان`}
                         </Typography>
-                    </Paper>
+                        <TextField inputProps={{style: {textAlign: "center", width: 200, fontWeight: "bolder" }, min: 1,max:data.count }}
+                                   type='number' aria-valuemin={1} defaultValue={1}>Count</TextField>
+                        <br/>
+                        <Button disabled={disable} className={classes.btn} variant="outlined" endIcon={<AddShoppingCart/> }>
+                             افزودن به سبد خرید   
+                        </Button>
+                        {disable && <Typography
+                            className={classes.notexist}>اتمام موجودی
+                        </Typography>}
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Typography className={classes.describtion} dangerouslySetInnerHTML={{__html:data.describtion}}></Typography>
+                    </Grid>
                 </Grid>
-                <Grid item xs={10} sm={8} md={6} lg={8}>
-                    <Paper  elevation={0}>
-                        <Typography className={classes.describtion}>{data.describtion}</Typography>
-                    </Paper>
-                </Grid>
-            </Grid>
+            </Paper>
         </div>
     );
 }
